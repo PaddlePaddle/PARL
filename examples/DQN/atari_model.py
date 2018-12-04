3dca946572c57b3d5af2c9c8e4720e8ab4c35b6e
zenghsh3 <zenghongsheng@baidu.com>
2018-12-04 21:22:23 +0800

    simplify code

--- examples/DQN/atari_model.py
+++ examples/DQN/atari_model.py
@@ -25,42 +25,26 @@ class AtariModel(Model):
         self.act_dim = act_dim
 
         self.conv1 = layers.conv2d(
-            num_filters=32,
-            filter_size=5,
-            stride=[1, 1],
-            padding=[2, 2],
-            act='relu')
+            num_filters=32, filter_size=5, stride=1, padding=2, act='relu')
         self.conv2 = layers.conv2d(
-            num_filters=32,
-            filter_size=5,
-            stride=[1, 1],
-            padding=[2, 2],
-            act='relu')
+            num_filters=32, filter_size=5, stride=1, padding=2, act='relu')
         self.conv3 = layers.conv2d(
-            num_filters=64,
-            filter_size=4,
-            stride=[1, 1],
-            padding=[1, 1],
-            act='relu')
+            num_filters=64, filter_size=4, stride=1, padding=1, act='relu')
         self.conv4 = layers.conv2d(
-            num_filters=64,
-            filter_size=3,
-            stride=[1, 1],
-            padding=[1, 1],
-            act='relu')
+            num_filters=64, filter_size=3, stride=1, padding=1, act='relu')
         self.fc1 = layers.fc(size=act_dim)
 
     def value(self, obs):
         obs = obs / 255.0
         out = self.conv1(obs)
         out = layers.pool2d(
-            input=out, pool_size=[2, 2], pool_stride=[2, 2], pool_type='max')
+            input=out, pool_size=2, pool_stride=2, pool_type='max')
         out = self.conv2(out)
         out = layers.pool2d(
-            input=out, pool_size=[2, 2], pool_stride=[2, 2], pool_type='max')
+            input=out, pool_size=2, pool_stride=2, pool_type='max')
         out = self.conv3(out)
         out = layers.pool2d(
-            input=out, pool_size=[2, 2], pool_stride=[2, 2], pool_type='max')
+            input=out, pool_size=2, pool_stride=2, pool_type='max')
         out = self.conv4(out)
         out = layers.flatten(out, axis=1)
         out = self.fc1(out)

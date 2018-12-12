200d900777716c1dc9471ba943903269cc96cd42
Joodo <wyattliang@gmail.com>
2018-12-12 10:47:34 +0800

    make code more clear

--- examples/QuickStart/train.py
+++ examples/QuickStart/train.py
@@ -75,7 +75,7 @@ def main():
     alg = PolicyGradient(model, hyperparas={'lr': LEARNING_RATE})
     agent = CartpoleAgent(alg, obs_dim=OBS_DIM, act_dim=ACT_DIM)
 
-    for i in range(501):
+    for i in range(500):
         obs_list, action_list, reward_list = run_train_episode(env, agent)
         logger.info("Episode {}, Reward Sum {}.".format(i, sum(reward_list)))
 
@@ -84,7 +84,7 @@ def main():
         batch_reward = calc_discount_norm_reward(reward_list)
 
         agent.learn(batch_obs, batch_action, batch_reward)
-        if i % 100 == 0:
+        if (i + 1) % 100 == 0:
             all_reward = run_evaluate_episode(env, agent)
             logger.info('Test reward: {}'.format(all_reward))
 

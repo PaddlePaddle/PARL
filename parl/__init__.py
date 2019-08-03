6c69c12bd7b2a9af42bbcd08d74857b25da76cff
zenghsh3 <zenghsh3@baidu.com>
2019-08-03 09:20:47 +0800

    release 1.2rc0

--- parl/__init__.py
+++ parl/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "1.1.1"
+__version__ = "1.2rc0"
 """
 generates new PARL python API
 """

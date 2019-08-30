002b95c72eca6f2956c4f0329f88ca9aaf8a42c8
fuyuwei01 <fuyuwei01@baidu.com>
2019-08-30 10:27:34 +0800

    combine two try except

--- parl/remote/job.py
+++ parl/remote/job.py
@@ -263,25 +263,14 @@ class Job(object):
         message = self.reply_socket.recv_multipart()
         tag = message[0]
         obj = None
+
         if tag == remote_constants.INIT_OBJECT_TAG:
             try:
                 cls = cloudpickle.loads(message[1])
-            except Exception as e:
-                traceback_str = str(traceback.format_exc())
-                error_str = str(e)
-                logger.error("traceback:\n{}".format(traceback_str))
-                self.reply_socket.send_multipart([
-                    remote_constants.EXCEPTION_TAG,
-                    to_byte(error_str + "\ntraceback:\n" + traceback_str)
-                ])
-                self.client_is_alive = False
-                return None
-
-            args, kwargs = cloudpickle.loads(message[2])
-            max_memory = to_str(message[3])
-            if max_memory != 'None':
-                self.max_memory = float(max_memory)
-            try:
+                args, kwargs = cloudpickle.loads(message[2])
+                max_memory = to_str(message[3])
+                if max_memory != 'None':
+                    self.max_memory = float(max_memory)
                 obj = cls(*args, **kwargs)
             except Exception as e:
                 traceback_str = str(traceback.format_exc())
@@ -293,7 +282,6 @@ class Job(object):
                 ])
                 self.client_is_alive = False
                 return None
-
             self.reply_socket.send_multipart([remote_constants.NORMAL_TAG])
         else:
             logger.error("Message from job {}".format(message))

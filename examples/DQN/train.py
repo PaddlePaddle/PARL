7941ff6978fa32a5a8f4d71a7c2c78408eff36e8
LI Yunxiang <1718613239banma@gmail.com>
2019-10-24 10:31:59 +0800

    add Double & Dueling DQN

--- examples/DQN/train.py
+++ examples/DQN/train.py
@@ -20,10 +20,9 @@ import os
 import parl
 from atari_agent import AtariAgent
 from atari_model import AtariModel
-from collections import deque
 from datetime import datetime
 from replay_memory import ReplayMemory, Experience
-from parl.utils import logger
+from parl.utils import tensorboard, logger
 from tqdm import tqdm
 from utils import get_player
 
@@ -34,7 +33,7 @@ CONTEXT_LEN = 4
 FRAME_SKIP = 4
 UPDATE_FREQ = 4
 GAMMA = 0.99
-LEARNING_RATE = 1e-3 * 0.5
+LEARNING_RATE = 3e-4
 
 
 def run_train_episode(env, agent, rpm):
@@ -42,6 +41,7 @@ def run_train_episode(env, agent, rpm):
     all_cost = []
     state = env.reset()
     steps = 0
+    lr = None
     while True:
         steps += 1
         context = rpm.recent_state()
@@ -57,7 +57,7 @@ def run_train_episode(env, agent, rpm):
                     args.batch_size)
                 batch_state = batch_all_state[:, :CONTEXT_LEN, :, :]
                 batch_next_state = batch_all_state[:, 1:, :, :]
-                cost = agent.learn(batch_state, batch_action, batch_reward,
+                cost, lr = agent.learn(batch_state, batch_action, batch_reward,
                                    batch_next_state, batch_isOver)
                 all_cost.append(float(cost))
         total_reward += reward
@@ -65,9 +65,9 @@ def run_train_episode(env, agent, rpm):
         if isOver:
             break
     if all_cost:
-        logger.info('[Train]total_reward: {}, mean_cost: {}'.format(
-            total_reward, np.mean(all_cost)))
-    return total_reward, steps
+       logger.info('[Train]total_reward: {}, mean_cost: {}'.format(
+           total_reward, np.mean(all_cost)))
+    return total_reward, steps, np.mean(all_cost), lr
 
 
 def run_evaluate_episode(env, agent):
@@ -93,27 +93,39 @@ def main():
     rpm = ReplayMemory(MEMORY_SIZE, IMAGE_SIZE, CONTEXT_LEN)
     act_dim = env.action_space.n
 
-    model = AtariModel(act_dim)
-    algorithm = parl.algorithms.DQN(
-        model, act_dim=act_dim, gamma=GAMMA, lr=LEARNING_RATE)
-    agent = AtariAgent(algorithm, act_dim=act_dim)
+    model = AtariModel(act_dim, args.algo)
+    if args.algo == 'Double':
+        algorithm = parl.algorithms.DDQN(
+            model, act_dim=act_dim, gamma=GAMMA)
+    elif args.algo in ['DQN', 'Dueling']:
+        algorithm = parl.algorithms.DQN(
+            model, act_dim=act_dim, gamma=GAMMA)
+    agent = AtariAgent(algorithm, act_dim=act_dim, 
+                       start_lr=LEARNING_RATE, total_step=args.train_total_steps)
 
-    with tqdm(total=MEMORY_WARMUP_SIZE) as pbar:
+    with tqdm(total=MEMORY_WARMUP_SIZE,
+              desc='[Replay Memory Warm Up]') as pbar:
         while rpm.size() < MEMORY_WARMUP_SIZE:
-            total_reward, steps = run_train_episode(env, agent, rpm)
+            total_reward, steps, _, _ = run_train_episode(env, agent, rpm)
             pbar.update(steps)
 
     # train
     test_flag = 0
     pbar = tqdm(total=args.train_total_steps)
-    recent_100_reward = []
     total_steps = 0
     max_reward = None
     while total_steps < args.train_total_steps:
         # start epoch
-        total_reward, steps = run_train_episode(env, agent, rpm)
+        total_reward, steps, loss, lr = run_train_episode(env, agent, rpm)
         total_steps += steps
         pbar.set_description('[train]exploration:{}'.format(agent.exploration))
+        tensorboard.add_scalar('dqn/score', total_reward, total_steps)
+        tensorboard.add_scalar('dqn/loss', loss, total_steps)   # mean of total loss
+        tensorboard.add_scalar('dqn/exploration', agent.exploration,
+                              total_steps)
+        if lr is not None:
+            tensorboard.add_scalar('dqn/learning_rate', lr,
+                              total_steps)
         pbar.update(steps)
 
         if total_steps // args.test_every_steps >= test_flag:
@@ -127,16 +139,23 @@ def main():
             logger.info(
                 "eval_agent done, (steps, eval_reward): ({}, {})".format(
                     total_steps, np.mean(eval_rewards)))
-
+            eval_test = np.mean(eval_rewards)
+            tensorboard.add_scalar('dqn/eval', eval_test,
+                                  total_steps)
+    
     pbar.close()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--rom', help='path of the rom of the atari game', required=True)
+        '--rom', 
+        help='path of the rom of the atari game', 
+        required=True
+        )
     parser.add_argument(
         '--batch_size', type=int, default=64, help='batch size for training')
+    parser.add_argument('--algo', default='DQN', help='DQN/Double/Dueling DQN')
     parser.add_argument(
         '--train_total_steps',
         type=int,
@@ -147,7 +166,8 @@ if __name__ == '__main__':
         type=int,
         default=100000,
         help='the step interval between two consecutive evaluations')
-
+    
     args = parser.parse_args()
-
+    rom_name = args.rom.split('/')[-1].split('.')[0]
+    logger.set_dir(os.path.join('./train_log', rom_name, args.algo+'_lr'))
     main()

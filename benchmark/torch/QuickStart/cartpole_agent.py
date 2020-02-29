90707a82f14415d89eb69dabbb6d5754395663dd
robot <zenghongsheng@baidu.com>
2020-02-29 15:44:00 +0800

    torch benchmark policy gradient

---
+++ benchmark/torch/QuickStart/cartpole_agent.py
@@ -0,0 +1,76 @@
+#   Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import parl
+import torch
+from torch.distributions import Categorical
+
+
+class CartpoleAgent(parl.Agent):
+    """Agent of Cartpole env.
+
+    Args:
+        algorithm(parl.Algorithm): algorithm used to solve the problem.
+
+    """
+
+    def __init__(self, algorithm):
+        self.algorithm = algorithm
+        self.device = torch.device("cuda" if torch.cuda.
+                                   is_available() else "cpu")
+
+    def sample(self, obs):
+        """Sample an action when given an observation
+
+        Args:
+            obs(np.float32): shape of (obs_dim)
+        
+        Returns:
+            action(int)
+        """
+        obs = torch.tensor(obs, device=self.device, dtype=torch.float)
+        action = self.algorithm.sample(obs)
+        return action.item()
+
+    def predict(self, obs):
+        """Predict an action when given an observation
+
+        Args:
+            obs(np.float32): shape of (obs_dim)
+        
+        Returns:
+            action(int)
+        """
+        obs = torch.tensor(obs, device=self.device, dtype=torch.float)
+        action = self.algorithm.predict(obs)
+        return action.item()
+
+    def learn(self, obs, action, reward):
+        """Update model with an episode data
+
+        Args:
+            obs(np.float32): shape of (N, obs_dim)
+            action(np.int64): shape of (N)
+            reward(np.float32): shape of (N)
+        
+        Returns:
+            loss(float)
+
+        """
+        obs = torch.tensor(obs, device=self.device, dtype=torch.float)
+        action = torch.tensor(action, device=self.device, dtype=torch.long)
+        reward = torch.tensor(reward, device=self.device, dtype=torch.float)
+
+        loss = self.algorithm.learn(obs, action, reward)
+        return loss.item()
---
+++ benchmark/torch/QuickStart/cartpole_model.py
@@ -0,0 +1,38 @@
+#   Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import parl
+
+
+class CartpoleModel(parl.Model):
+    """ Linear network to solve Cartpole problem.
+    
+    Args:
+        obs_dim (int): Dimension of observation space.
+        act_dim (int): Dimension of action space.
+    """
+
+    def __init__(self, obs_dim, act_dim):
+        super(CartpoleModel, self).__init__()
+        hid1_size = act_dim * 10
+        self.fc1 = nn.Linear(obs_dim, hid1_size)
+        self.fc2 = nn.Linear(hid1_size, act_dim)
+
+    def forward(self, x):
+        out = torch.tanh(self.fc1(x))
+        prob = F.softmax(self.fc2(out), dim=-1)
+        return prob

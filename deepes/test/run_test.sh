ff92144fbf19f5126c8e2ab11bc738332df8ddf1
likejiao <likejiao@baidu.com>
2020-03-25 21:11:54 +0800

    remove the commented lines and empty functions.

--- deepes/test/run_test.sh
+++ deepes/test/run_test.sh
@@ -8,20 +8,10 @@ protoc deepes.proto --cpp_out ./
 mv deepes.pb.h ./include
 mv deepes.pb.cc ./src
 
-#---------------libtorch-------------#
-# if [ ! -d "./libtorch" ];then
-#  	echo "Cannot find the torch library: ./libtorch"
-# 	echo "Downloading Torch library"
-# 	wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.4.0%2Bcpu.zip
-# 	unzip libtorch-cxx11-abi-shared-with-deps-1.4.0+cpu.zip
-#  	rm libtorch-cxx11-abi-shared-with-deps-1.4.0+cpu.zip
-#  	echo "Torch library Downloaded"
-# fi
-
 #----------------build---------------#
 rm -rf build
 mkdir build
 cd build
-cmake ../test  #  -DWITH_TORCH=ON
+cmake ../test  # -DWITH_TORCH=ON
 make -j10
 ./unit_test_main
--- deepes/test/src/test_utils.cc
+++ deepes/test/src/test_utils.cc
@@ -19,35 +19,7 @@
 namespace DeepES {
 
 // The fixture for testing class or file Utils.
-class UtilsTest : public ::testing::Test {
- protected:
-  // You can remove any or all of the following functions if their bodies would
-  // be empty.
-
-  UtilsTest() {
-     // You can do set-up work for each test here.
-  }
-
-  ~UtilsTest() override {
-     // You can do clean-up work that doesn't throw exceptions here.
-  }
-
-  // If the constructor and destructor are not enough for setting up
-  // and cleaning up each test, you can define the following methods:
-
-  void SetUp() override {
-     // Code here will be called immediately after the constructor (right
-     // before each test).
-  }
-
-  void TearDown() override {
-     // Code here will be called immediately after each test (right
-     // before the destructor).
-  }
-
-  // Class members declared here can be used by all tests in the test suite
-  // for Utils.
-};
+class UtilsTest : public ::testing::Test {};
 
 // Tests that the Utils::compute_centered_rank() method.
 TEST_F(UtilsTest, Method_compute_centered_ranks) {
@@ -55,11 +27,6 @@ TEST_F(UtilsTest, Method_compute_centered_ranks) {
   std::vector<float> reward_vec(a, a+5);
   EXPECT_EQ(compute_centered_ranks(reward_vec), true);
 }
-
-// // Tests that Utils another method Xyz.
-// TEST_F(UtilsTest, Method_Xyz) {
-//   // Exercises the Method Xyz of Utils.
-// }
 
 
 }  // namespace project

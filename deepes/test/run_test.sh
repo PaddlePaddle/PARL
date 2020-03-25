76b74940f18cb14044743cdb9a7e4c68914d5c24
likejiao <likejiao@baidu.com>
2020-03-25 16:27:15 +0800

    update Dockerfile

--- deepes/test/run_test.sh
+++ deepes/test/run_test.sh
@@ -10,12 +10,12 @@ mv deepes.pb.cc ./src
 
 #---------------libtorch-------------#
 # if [ ! -d "./libtorch" ];then
-#   echo "Cannot find the torch library: ./libtorch"
+#  	echo "Cannot find the torch library: ./libtorch"
 # 	echo "Downloading Torch library"
 # 	wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.4.0%2Bcpu.zip
 # 	tar zxvf libtorch-cxx11-abi-shared-with-deps-1.4.0+cpu.zip
-#   rm libtorch-cxx11-abi-shared-with-deps-1.4.0+cpu.zip
-#   echo "Torch library Downloaded"
+#  	rm libtorch-cxx11-abi-shared-with-deps-1.4.0+cpu.zip
+#  	echo "Torch library Downloaded"
 # fi
 
 #----------------build---------------#

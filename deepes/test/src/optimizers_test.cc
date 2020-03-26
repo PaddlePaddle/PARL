4824980610da76ab8e3aa83d68f285e801f9f22a
likejiao <likejiao@baidu.com>
2020-03-26 15:50:12 +0800

    use optimizer_factory and add fail unittest case

--- deepes/test/src/optimizers_test.cc
+++ deepes/test/src/optimizers_test.cc
@@ -14,16 +14,18 @@
 
 #include "gtest/gtest.h"
 #include <vector>
-#include "optimizer.h"
-#include "sgd_optimizer.h"
-#include "adam_optimizer.h"
+#include "optimizer_factory.h"
 #include <memory>
 
 namespace DeepES {
 
-// Tests that the Optimizers::compute_centered_rank() method.
 TEST(SGDOptimizersTest, Method_update) {
-	std::shared_ptr<Optimizer> optimizer = std::make_shared<SGDOptimizer>(1.0);
+    std::shared_ptr<DeepESConfig> config = std::make_shared<DeepESConfig>();
+	auto optimizer_config = config->mutable_optimizer();
+	optimizer_config->set_base_lr(1.0);
+	optimizer_config->set_type("sgd");
+	std::shared_ptr<Optimizer> optimizer = create_optimizer(config->optimizer());
+	// std::shared_ptr<Optimizer> optimizer = std::make_shared<SGDOptimizer>(1.0);
 	float sgd_wei[10]  = { 0.0       , 0.0       , 0.04216444, 0.0511456 , 0.04231584, 0.01089015, 0.06569759, 0.00127421,-0.00092832, 0.01128081};
 	float sgd_grad[10] = {-0.11992419,-0.0       , 0.07681337,-0.06616384, 0.00249889, 0.01158612,-0.3067452 , 0.36048946,-0.15820622,-0.20014143};
 	float sgd_new[10]  = { 0.01199242, 0.0       , 0.0344831 , 0.05776198, 0.04206595, 0.00973154, 0.09637211,-0.03477474, 0.014892306, 0.03129495};
@@ -35,9 +37,12 @@ TEST(SGDOptimizersTest, Method_update) {
 	EXPECT_FALSE(optimizer->update(sgd_wei, sgd_grad, 9, "test"));
 }
 
-// Tests that the Optimizers::compute_centered_rank() method.
 TEST(AdamOptimizersTest, Method_update) {
-	std::shared_ptr<Optimizer> optimizer = std::make_shared<AdamOptimizer>(1.0);
+    std::shared_ptr<DeepESConfig> config = std::make_shared<DeepESConfig>();
+	auto optimizer_config = config->mutable_optimizer();
+	optimizer_config->set_base_lr(1.0);
+	optimizer_config->set_type("adam");
+	std::shared_ptr<Optimizer> optimizer = create_optimizer(config->optimizer());
 	float adam_wei[10]  = { 0.0       , 0.0       , 0.04216444, 0.0511456 , 0.04231584, 0.01089015, 0.06569759, 0.00127421,-0.00092832, 0.01128081};
 	float adam_grad[10] = {-0.11992419,-0.0       , 0.07681337,-0.06616384, 0.00249889, 0.01158612,-0.3067452 , 0.36048946,-0.15820622,-0.20014143};
 	float adam_new[10]  = { 0.99999736, 0.        ,-0.95783144, 1.05114082,-0.95755763,-0.98908256, 1.06569656,-0.99872491, 0.99906968, 1.01127923};

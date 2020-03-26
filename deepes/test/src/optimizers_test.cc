6bc78d3dc854c0d057986239b3d5488b79fc281b
likejiao <likejiao@baidu.com>
2020-03-26 17:47:10 +0800

    remove comment line

--- deepes/test/src/optimizers_test.cc
+++ deepes/test/src/optimizers_test.cc
@@ -25,7 +25,6 @@ TEST(SGDOptimizersTest, Method_update) {
 	optimizer_config->set_base_lr(1.0);
 	optimizer_config->set_type("sgd");
 	std::shared_ptr<Optimizer> optimizer = create_optimizer(config->optimizer());
-	// std::shared_ptr<Optimizer> optimizer = std::make_shared<SGDOptimizer>(1.0);
 	float sgd_wei[10]  = { 0.0       , 0.0       , 0.04216444, 0.0511456 , 0.04231584, 0.01089015, 0.06569759, 0.00127421,-0.00092832, 0.01128081};
 	float sgd_grad[10] = {-0.11992419,-0.0       , 0.07681337,-0.06616384, 0.00249889, 0.01158612,-0.3067452 , 0.36048946,-0.15820622,-0.20014143};
 	float sgd_new[10]  = { 0.01199242, 0.0       , 0.0344831 , 0.05776198, 0.04206595, 0.00973154, 0.09637211,-0.03477474, 0.014892306, 0.03129495};

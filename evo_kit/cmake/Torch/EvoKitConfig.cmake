a95e470bbcd962b8347f692c98316e7c6b337f0c
likejiao <likejiao@baidu.com>
2020-04-23 21:33:55 +0800

    set library path EVOKIT_LIBRARY

--- evo_kit/cmake/Torch/EvoKitConfig.cmake
+++ evo_kit/cmake/Torch/EvoKitConfig.cmake
@@ -3,6 +3,14 @@
 #
 # Finds the EvoKit library
 #
+# This will define the following variables:
+#
+#   EVOKIT_FOUND        -- True if the system has the EvoKit library
+#   EVOKIT_INCLUDE_DIRS -- The include directories for EvoKit
+#   EVOKIT_LIBRARY    -- Libraries to link against
+#
+# and the following imported targets:
+#
 #   EvoKit
 
 include(FindPackageHandleStandardArgs)
@@ -26,10 +34,12 @@ else()
     ${EVOKIT_INSTALL_PREFIX}/torch)
 endif()
 
-message(STATUS "EVOKIT_INSTALL_PREFIX: ${EVOKIT_INSTALL_PREFIX}")
-
 find_library(EVOKIT_LIBRARY libEvoKit_torch.a PATHS "${EVOKIT_INSTALL_PREFIX}/lib")
+
 include_directories("${EVOKIT_INSTALL_PREFIX}/torch")
 include_directories("${EVOKIT_INSTALL_PREFIX}/include")
+
 find_package_handle_standard_args(EvoKit DEFAULT_MSG EVOKIT_LIBRARY EVOKIT_INCLUDE_DIRS)
 message(STATUS "EVOKIT_FOUND: ${EVOKIT_FOUND}")
+message(STATUS "EVOKIT_INCLUDE_DIRS: ${EVOKIT_INCLUDE_DIRS}")
+message(STATUS "EVOKIT_LIBRARY: ${EVOKIT_LIBRARY}")

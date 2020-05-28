d384a5b7f2924bbfca271f9edd9430ee6afd1fb5
likejiao <likejiao@baidu.com>
2020-05-28 11:12:05 +0800

    yapf

--- examples/tutorials/lesson5/ddpg_cartpole/env.py
+++ examples/tutorials/lesson5/ddpg_cartpole/env.py
@@ -11,9 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 #-*- coding: utf-8 -*-
-
 """
 Classic cart-pole system implemented by Rich Sutton et al.
 Copied from http://incompleteideas.net/sutton/book/code/pole.c

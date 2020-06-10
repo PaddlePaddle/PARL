1cf17a579ce0c375b38567ff810e424b7c8c87c7
rical730 <likejiao@baidu.com>
2020-06-10 19:50:22 +0800

    add tutorials (#270)
    * add tutorials
    
    * yapf
    
    * yapf
    
    * copyright
    
    * yapf
    
    * update tutorial lesson5
    
    * delete drawing code
    
    * yapf
    
    * remove action_mapping
    
    * update dqn and add README
    
    * update
    
    * update
    
    * yapf

---
+++ examples/tutorials/lesson4/policy_gradient/model.py
@@ -0,0 +1,32 @@
+#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#-*- coding: utf-8 -*-
+
+import parl
+from parl import layers
+
+
+class Model(parl.Model):
+    def __init__(self, act_dim):
+        act_dim = act_dim
+        hid1_size = act_dim * 10
+
+        self.fc1 = layers.fc(size=hid1_size, act='tanh')
+        self.fc2 = layers.fc(size=act_dim, act='softmax')
+
+    def forward(self, obs):  # 可直接用 model = Model(5); model(obs)调用
+        out = self.fc1(obs)
+        out = self.fc2(out)
+        return out

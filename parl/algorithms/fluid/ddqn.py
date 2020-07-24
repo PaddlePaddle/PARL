3e30aa46b317848cb6456e15131e5f7d64a207e2
tanzeyy <tan_ze@outlook.com>
2020-07-24 15:05:25 +0800

    update ddqn implementation

--- parl/algorithms/fluid/ddqn.py
+++ parl/algorithms/fluid/ddqn.py
@@ -70,26 +70,14 @@ class DDQN(Algorithm):
         pred_action_value = layers.reduce_sum(
             layers.elementwise_mul(action_onehot, pred_value), dim=1)
 
-        # choose acc. to behavior network
+        # calculate the target q value
         next_action_value = self.model.value(next_obs)
         greedy_action = layers.argmax(next_action_value, axis=-1)
-
-        # calculate the target q value with target network
-        batch_size = layers.cast(layers.shape(greedy_action)[0], dtype='int64')
-        range_tmp = layers.range(
-            start=0, end=batch_size, step=1, dtype='int64') * self.act_dim
-        a_indices = range_tmp + greedy_action
-        a_indices = layers.cast(a_indices, dtype='int32')
+        greedy_action = layers.unsqueeze(greedy_action, axes=[1])
+        greedy_action_onehot = layers.one_hot(greedy_action, self.act_dim)
         next_pred_value = self.target_model.value(next_obs)
-        next_pred_value = layers.reshape(
-            next_pred_value, shape=[
-                -1,
-            ])
-        max_v = layers.gather(next_pred_value, a_indices)
-        max_v = layers.reshape(
-            max_v, shape=[
-                -1,
-            ])
+        max_v = layers.reduce_sum(
+            greedy_action_onehot * next_pred_value, dim=1)
         max_v.stop_gradient = True
 
         target = reward + (

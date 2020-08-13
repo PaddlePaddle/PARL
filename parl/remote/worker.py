4a8bb41a63cd5614b1ba8e87db642764d13ce8f6
liuyuecheng-github <liuy150617@163.com>
2020-08-12 18:17:44 -0700

    add function to get and set attributes of remote models

--- parl/remote/worker.py
+++ parl/remote/worker.py
@@ -208,10 +208,9 @@ class Worker(object):
         # Redirect the output to DEVNULL
         FNULL = open(os.devnull, 'w')
         for _ in range(job_num):
-            subprocess.Popen(command)
+            subprocess.Popen(command, stdout=FNULL, stderr=subprocess.STDOUT)
         FNULL.close()
-            # subprocess.Popen(command, stdout=subprocess.STDOUT, stderr=subprocess.STDOUT)
-
+        # subprocess.Popen(command, stdout=subprocess.STDOUT, stderr=subprocess.STDOUT)
 
         new_jobs = []
         for _ in range(job_num):

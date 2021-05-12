9fa49a18485557baeebc563385d3cd5da6902d3f
likejiao <likejiao@baidu.com>
2021-05-12 11:35:32 +0800

    add torch_based maddpg

---
+++ benchmark/torch/maddpg/simple_agent.py
@@ -0,0 +1,124 @@
+#   Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import parl
+import torch
+import numpy as np
+from parl.utils import ReplayMemory
+from parl.utils import machine_info, get_gpu_count
+
+
+class MAAgent(parl.Agent):
+    def __init__(self,
+                 algorithm,
+                 agent_index=None,
+                 obs_dim_n=None,
+                 act_dim_n=None,
+                 batch_size=None,
+                 speedup=False):
+        assert isinstance(agent_index, int)
+        assert isinstance(obs_dim_n, list)
+        assert isinstance(act_dim_n, list)
+        assert isinstance(batch_size, int)
+        assert isinstance(speedup, bool)
+        self.agent_index = agent_index
+        self.obs_dim_n = obs_dim_n
+        self.act_dim_n = act_dim_n
+        self.batch_size = batch_size
+        self.speedup = speedup
+        self.n = len(act_dim_n)
+
+        self.memory_size = int(1e5)
+        self.min_memory_size = batch_size * 25  # batch_size * args.max_episode_len
+        self.rpm = ReplayMemory(
+            max_size=self.memory_size,
+            obs_dim=self.obs_dim_n[agent_index],
+            act_dim=self.act_dim_n[agent_index])
+        self.global_train_step = 0
+        self.device = torch.device("cuda" if torch.cuda.
+                                   is_available() else "cpu")
+
+        if machine_info.is_gpu_available():
+            assert get_gpu_count() == 1, 'Only support training in single GPU,\
+                    Please set environment variable: `export CUDA_VISIBLE_DEVICES=[GPU_ID_TO_USE]` .'
+
+        super(MAAgent, self).__init__(algorithm)
+
+        # Attention: In the beginning, sync target model totally.
+        self.alg.sync_target(decay=0)
+
+    def predict(self, obs, use_target_model=False):
+        """ predict action by model or target_model
+        """
+        obs = torch.FloatTensor(obs.reshape(1, -1)).to(self.device)
+        act = self.alg.predict(obs, use_target_model=use_target_model)
+        act_numpy = act.cpu().detach().numpy().flatten()
+        return act_numpy
+
+    def learn(self, agents):
+        self.global_train_step += 1
+
+        # only update parameter every 100 steps
+        if self.global_train_step % 100 != 0:
+            return 0.0
+
+        if self.rpm.size() <= self.min_memory_size:
+            return 0.0
+
+        batch_obs_n = []
+        batch_act_n = []
+        batch_obs_next_n = []
+
+        rpm_sample_index = self.rpm.make_index(self.batch_size)
+        for i in range(self.n):
+            batch_obs, batch_act, _, batch_obs_next, _ \
+                = agents[i].rpm.sample_batch_by_index(rpm_sample_index)
+            batch_obs_n.append(batch_obs)
+            batch_act_n.append(batch_act)
+            batch_obs_next_n.append(batch_obs_next)
+        _, _, batch_rew, _, batch_isOver = self.rpm.sample_batch_by_index(
+            rpm_sample_index)
+        batch_obs_n = [
+            torch.FloatTensor(obs).to(self.device) for obs in batch_obs_n
+        ]
+        batch_act_n = [
+            torch.FloatTensor(act).to(self.device) for act in batch_act_n
+        ]
+        batch_rew = torch.FloatTensor(batch_rew).to(self.device)
+        batch_isOver = torch.FloatTensor(batch_isOver).to(self.device)
+
+        # compute target q
+        target_act_next_n = []
+        batch_obs_next_n = [
+            torch.FloatTensor(obs).to(self.device) for obs in batch_obs_next_n
+        ]
+        for i in range(self.n):
+            target_act_next = agents[i].alg.predict(
+                batch_obs_next_n[i], use_target_model=True)
+            target_act_next = target_act_next.detach()
+            target_act_next_n.append(target_act_next)
+        target_q_next = self.alg.Q(
+            batch_obs_next_n, target_act_next_n, use_target_model=True)
+        target_q = batch_rew + self.alg.gamma * (
+            1.0 - batch_isOver) * target_q_next.detach()
+
+        # learn
+        critic_cost = self.alg.learn(batch_obs_n, batch_act_n, target_q)
+        critic_cost = critic_cost.cpu().detach().numpy()
+
+        self.alg.sync_target()
+        return critic_cost
+
+    def add_experience(self, obs, act, reward, next_obs, terminal):
+        self.rpm.append(obs, act, reward, next_obs, terminal)
---
+++ parl/algorithms/torch/maddpg.py
@@ -0,0 +1,164 @@
+#   Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import parl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from copy import deepcopy
+
+__all__ = ['MADDPG']
+
+from parl.core.torch.policy_distribution import SoftCategoricalDistribution
+from parl.core.torch.policy_distribution import SoftMultiCategoricalDistribution
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def SoftPDistribution(logits, act_space):
+    """Args:
+            logits: the output of policy model
+            act_space: action space, must be gym.spaces.Discrete or multiagent.multi_discrete.MultiDiscrete
+
+        Return:
+            instance of SoftCategoricalDistribution or SoftMultiCategoricalDistribution
+    """
+    # is instance of gym.spaces.Discrete
+    if (hasattr(act_space, 'n')):
+        return SoftCategoricalDistribution(logits)
+    # is instance of multiagent.multi_discrete.MultiDiscrete
+    elif (hasattr(act_space, 'num_discrete_space')):
+        return SoftMultiCategoricalDistribution(logits, act_space.low,
+                                                act_space.high)
+    else:
+        raise AssertionError("act_space must be instance of \
+            gym.spaces.Discrete or multiagent.multi_discrete.MultiDiscrete")
+
+
+class MADDPG(parl.Algorithm):
+    def __init__(self,
+                 model,
+                 agent_index=None,
+                 act_space=None,
+                 gamma=None,
+                 tau=None,
+                 actor_lr=None,
+                 critic_lr=None):
+        """  MADDPG algorithm
+
+        Args:
+            model (parl.Model): forward network of actor and critic.
+                                The function get_actor_params() of model should be implemented.
+            agent_index: index of agent, in multiagent env
+            act_space: action_space, gym space
+            gamma (float): discounted factor for reward computation.
+            tau (float): decay coefficient when updating the weights of self.target_model with self.model
+            critic_lr (float): learning rate of the critic model
+            actor_lr (float): learning rate of the actor model
+        """
+
+        assert isinstance(agent_index, int)
+        assert isinstance(act_space, list)
+        assert isinstance(gamma, float)
+        assert isinstance(tau, float)
+        assert isinstance(actor_lr, float)
+        assert isinstance(critic_lr, float)
+        self.agent_index = agent_index
+        self.act_space = act_space
+        self.gamma = gamma
+        self.tau = tau
+        self.actor_lr = actor_lr
+        self.critic_lr = critic_lr
+
+        self.model = model.to(device)
+        self.target_model = deepcopy(model)
+        self.sync_target(0)
+
+        self.actor_optimizer = torch.optim.Adam(
+            lr=self.actor_lr, params=self.model.get_actor_params())
+        self.critic_optimizer = torch.optim.Adam(
+            lr=self.critic_lr, params=self.model.get_critic_params())
+
+    def predict(self, obs, use_target_model=False):
+        """ input:  
+                obs: observation, shape([B] + shape of obs_n[agent_index])
+            output: 
+                act: action, shape([B] + shape of act_n[agent_index])
+        """
+        if use_target_model:
+            policy = self.target_model.policy(obs)
+        else:
+            policy = self.model.policy(obs)
+        action = SoftPDistribution(
+            logits=policy,
+            act_space=self.act_space[self.agent_index]).sample()
+        return action
+
+    def Q(self, obs_n, act_n, use_target_model=False):
+        """ input:  
+                obs_n: all agents' observation, shape([B] + shape of obs_n)
+                act_n: all agents' action, shape([B] + shape of act_n)
+            output: 
+                Q: Q value of this agent, shape([B])
+        """
+        if use_target_model:
+            return self.target_model.value(obs_n, act_n)
+        else:
+            return self.model.value(obs_n, act_n)
+
+    def learn(self, obs_n, act_n, target_q):
+        """ update actor and critic model with MADDPG algorithm
+        """
+        actor_cost = self._actor_learn(obs_n, act_n)
+        critic_cost = self._critic_learn(obs_n, act_n, target_q)
+        return critic_cost
+
+    def _actor_learn(self, obs_n, act_n):
+        i = self.agent_index
+
+        this_policy = self.model.policy(obs_n[i])
+        sample_this_action = SoftPDistribution(
+            logits=this_policy,
+            act_space=self.act_space[self.agent_index]).sample()
+
+        # action_input_n = deepcopy(act_n)
+        action_input_n = act_n + []
+        action_input_n[i] = sample_this_action
+        eval_q = self.Q(obs_n, action_input_n)
+        act_cost = torch.mean(-1.0 * eval_q)
+
+        act_reg = torch.mean(torch.square(this_policy))
+
+        cost = act_cost + act_reg * 1e-3
+
+        self.actor_optimizer.zero_grad()
+        cost.backward()
+        torch.nn.utils.clip_grad_norm_(self.model.get_actor_params(), 0.5)
+        self.actor_optimizer.step()
+        return cost
+
+    def _critic_learn(self, obs_n, act_n, target_q):
+        pred_q = self.Q(obs_n, act_n)
+        cost = F.mse_loss(pred_q, target_q)
+
+        self.critic_optimizer.zero_grad()
+        cost.backward()
+        torch.nn.utils.clip_grad_norm_(self.model.get_critic_params(), 0.5)
+        self.critic_optimizer.step()
+        return cost
+
+    def sync_target(self, decay=None):
+        if decay is None:
+            decay = 1.0 - self.tau
+        self.model.sync_weights_to(self.target_model, decay=decay)

a931de4eca6f9fd4d6b31104f97828e0a439a43f
zenghsh3 <zenghongsheng@baidu.com>
2021-05-25 22:41:31 +0800

    release 2.0.0rc0 (#631)

--- parl/__init__.py
+++ parl/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "1.4.3"
+__version__ = "2.0.0rc0"
 """
 generates new PARL python API
 """

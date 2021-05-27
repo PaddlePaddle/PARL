e60c7ec74831fdd06e106f1acdb55b002f768790
swag1ong <zhaoyang.zhu@mail.utoronto.ca>
2021-05-27 15:05:30 +0800

    add imagconvert

--- docs/conf.py
+++ docs/conf.py
@@ -54,6 +54,7 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.mathjax',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.imgconverter'
 ]
 
 # Add any paths that contain templates here, relative to this directory.

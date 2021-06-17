755d5af83164225f6a991e118ac2077a58437d4e
likejiao <likejiao@baidu.com>
2021-06-17 19:08:12 +0800

    add paddle_based maddpg

--- parl/algorithms/paddle/__init__.py
+++ parl/algorithms/paddle/__init__.py
@@ -21,3 +21,4 @@ from parl.algorithms.paddle.oac import *
 from parl.algorithms.paddle.qmix import *
 from parl.algorithms.paddle.a2c import *
 from parl.algorithms.paddle.ddqn import *
+from parl.algorithms.paddle.maddpg import *

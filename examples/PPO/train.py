74922367327268513123bba49051065307b750d9
swag1ong <zhaoyang.zhu@mail.utoronto.ca>
2021-06-21 14:48:28 +0800

    update paddle ppo

--- examples/PPO/train.py
+++ examples/PPO/train.py
@@ -15,57 +15,84 @@
 # modified from https://github.com/ikostrikov/pytorch-a2c-ppo-acktr-gail
 
 from collections import deque
-
 import numpy as np
 import paddle
-
-import utils
-from arguments import get_args
-from wrapper import make_env
 from mujoco_model import MujocoModel
-from parl.algorithms import PPO
 from mujoco_agent import MujocoAgent
 from storage import RolloutStorage
-from evaluation import evaluate
-from parl.utils import logger, summary
+from parl.algorithms import PPO
+from parl.env.mujoco_wrappers import make_env, get_vec_normalize
+from parl.utils import summary
+import argparse
+
+LR = 3e-4
+GAMMA = 0.99
+EPS = 1e-5  # Adam optimizer epsilon (default: 1e-5)
+GAE_LAMBDA = 0.95  # Lambda parameter for calculating N-step advantage
+ENTROPY_COEF = 0  # Entropy coefficient (ie. c_2 in the paper)
+VALUE_LOSS_COEF = 0.5  # Value loss coefficient (ie. c_1 in the paper)
+MAX_GRAD_NROM = 0.5  # Max gradient norm for gradient clipping
+NUM_STEPS = 2048  # data collecting time steps (ie. T in the paper)
+PPO_EPOCH = 10  # number of epochs for updating using each T data (ie K in the paper)
+CLIP_PARAM = 0.2  # epsilon in clipping loss (ie. clip(r_t, 1 - epsilon, 1 + epsilon))
+
+# Logging Params
+LOG_INTERVAL = 1
+
+
+def evaluate(agent, ob_rms, env_name, seed):
+    if seed is not None:
+        seed += 1
+
+    eval_envs = make_env(env_name, seed, None)
+    vec_norm = get_vec_normalize(eval_envs)
+    if vec_norm is not None:
+        vec_norm.eval()
+        vec_norm.ob_rms = ob_rms
+
+    eval_episode_rewards = []
+    obs = eval_envs.reset()
+
+    while len(eval_episode_rewards) < 10:
+        action = agent.predict(obs)
+
+        # Observe reward and next obs
+        obs, _, done, infos = eval_envs.step(action)
+
+        for info in infos:
+            if 'episode' in info.keys():
+                eval_episode_rewards.append(info['episode']['r'])
+
+    eval_envs.close()
+
+    print(" Evaluation using {} episodes: mean reward {:.5f}\n".format(
+        len(eval_episode_rewards), np.mean(eval_episode_rewards)))
+    return np.mean(eval_episode_rewards)
 
 
 def main():
-    args = get_args()
     paddle.seed(args.seed)
-    envs = make_env(args.env_name, args.seed, args.gamma)
+    envs = make_env(args.env, args.seed, GAMMA)
 
-    # model
     model = MujocoModel(envs.observation_space.shape[0],
                         envs.action_space.shape[0])
 
-    # algorithm
-    algorithm = PPO(
-        model,
-        args.clip_param,
-        args.value_loss_coef,
-        args.entropy_coef,
-        initial_lr=args.lr,
-        eps=args.eps,
-        max_grad_norm=args.max_grad_norm)
+    algorithm = PPO(model, CLIP_PARAM, VALUE_LOSS_COEF, ENTROPY_COEF, LR, EPS,
+                    MAX_GRAD_NROM)
 
-    # agent
     agent = MujocoAgent(algorithm)
-    rollouts = RolloutStorage(args.num_steps, envs.observation_space.shape[0],
+
+    rollouts = RolloutStorage(NUM_STEPS, envs.observation_space.shape[0],
                               envs.action_space.shape[0])
 
     obs = envs.reset()
     rollouts.obs[0] = np.copy(obs)
+
     episode_rewards = deque(maxlen=10)
-    num_updates = int(args.num_env_steps) // args.num_steps
 
+    num_updates = int(args.train_total_steps) // NUM_STEPS
     for j in range(num_updates):
-        if args.use_linear_lr_decay:
-            # decrease learning rate linearly
-            utils.update_linear_schedule(algorithm.optimizer, j, num_updates,
-                                         args.lr)
-
-        for step in range(args.num_steps):
+        for step in range(NUM_STEPS):
             # Sample actions
             value, action, action_log_prob = agent.sample(rollouts.obs[step])
 
@@ -89,13 +116,13 @@ def main():
         next_value = agent.value(rollouts.obs[-1])
 
         value_loss, action_loss, dist_entropy = agent.learn(
-            next_value, args.gamma, args.gae_lambda, args.ppo_epoch,
-            args.num_mini_batch, rollouts)
+            next_value, GAMMA, GAE_LAMBDA, PPO_EPOCH, args.batch_size,
+            rollouts)
 
         rollouts.after_update()
 
-        if j % args.log_interval == 0 and len(episode_rewards) > 1:
-            total_num_steps = (j + 1) * args.num_steps
+        if j % LOG_INTERVAL == 0 and len(episode_rewards) > 1:
+            total_num_steps = (j + 1) * NUM_STEPS
             print(
                 "Updates {}, num timesteps {},\n Last {} training episodes: mean/median reward {:.1f}/{:.1f}, min/max reward {:.1f}/{:.1f}\n"
                 .format(j, total_num_steps, len(episode_rewards),
@@ -103,18 +130,37 @@ def main():
                         np.min(episode_rewards), np.max(episode_rewards),
                         dist_entropy, value_loss, action_loss))
 
-        if (args.eval_interval is not None and len(episode_rewards) > 1
-                and j % args.eval_interval == 0):
-            ob_rms = utils.get_vec_normalize(envs).ob_rms
-            eval_mean_reward = evaluate(agent, ob_rms, args.env_name,
-                                        args.seed)
-
+        if (args.test_every_steps is not None and len(episode_rewards) > 1
+                and j % args.test_every_steps == 0):
+            ob_rms = get_vec_normalize(envs).ob_rms
+            eval_mean_reward = evaluate(agent, ob_rms, args.env, args.seed)
             summary.add_scalar('ppo/mean_validation_rewards', eval_mean_reward,
-                               (j + 1) * args.num_steps)
-            logger.info(
-                "eval_agent done, (steps, eval_reward): ({}, {})".format(
-                    (j + 1) * args.num_steps, eval_mean_reward))
+                               (j + 1) * NUM_STEPS)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='RL')
+    parser.add_argument(
+        '--seed', type=int, default=1, help='random seed (default: 1)')
+    parser.add_argument(
+        '--batch_size',
+        type=int,
+        default=32,
+        help='number of batches for ppo (default: 32)')
+    parser.add_argument(
+        '--test_every_steps',
+        type=int,
+        default=10,
+        help='eval interval (default: 10)')
+    parser.add_argument(
+        '--train_total_steps',
+        type=int,
+        default=10e5,
+        help='number of total time steps to train (default: 10e5)')
+    parser.add_argument(
+        '--env',
+        default='Hopper-v1',
+        help='environment to train on (default: Hopper-v1)')
+    args = parser.parse_args()
+
     main()

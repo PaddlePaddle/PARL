7a910d3c586b01f18523ad45e4f8ecc7b49a249f
zenghsh3 <zenghongsheng@baidu.com>
2021-08-27 14:51:00 +0800

    Baselines of grid dispatching competition (#709)
    * add baselines for grid dispatching competition
    
    * update doc
    
    * remove model
    
    * update link
    
    * fix yapf
    
    * refine code and doc
    
    * refine doc

---
+++ examples/Baselines/GridDispatch_competition/paddle/README.md
@@ -0,0 +1,61 @@
+## SAC baseline for grid dispatching competition
+
+In this example, we provide a distributed SAC baseline based on PARL and paddlepaddle for the [grid dispatching competition](https://aistudio.baidu.com/aistudio/competition/detail/111) task.
+
+### Dependencies
+* Linux
+* python3.6+
+* paddlepaddle >= 2.1.0
+* parl >= 2.0.0
+
+### Computing resource requirements
+* 1 GPU + 6 CPUs
+
+### Training
+
+1. Download the pretrained model (trained with fixed first 288 timesteps data) in the current directory. (filename: `paddle_pretrain_model`)
+
+[Baidu Pan](https://pan.baidu.com/s/1R-4EWIgNr2YogbJnMXk4Cg) (password: hwkb)
+
+2. Copy all files of `gridsim` (the competition package) to the current directory.
+```bash
+# For example:
+cp -r /XXX/gridsim/* .
+```
+
+2. Update the data path for distributed training (Using an absoluate path).
+```bash
+export PWD=`pwd`
+python yml_creator.py --dataset_path $PWD/data
+```
+
+
+3. Set the environment variable of PARL and gridsim.
+```bash
+export PARL_BACKEND=paddle
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib64
+```
+
+4. Start xparl cluster
+
+```bash
+# You can change following `cpu_num` and `args.actor_num` in the train.py based on the CPU number of your machine.
+# Note that you only need to start the cluster once.
+
+xparl start --port 8010 --cpu_num 6
+```
+
+5. start training. 
+
+```bash
+python train.py --actor_num 6
+```
+
+6. Visualize the training curve and other information.
+```
+tensorboard --logdir .
+```
+
+### Performance
+The result after training one hour with 1 GPU and 6 CPUs.
+![learning curve](https://raw.githubusercontent.com/benchmarking-rl/PARL-experiments/master/Baselines/GridDispatch_competition/paddle/result.png)
---
+++ examples/Baselines/GridDispatch_competition/torch/grid_model.py
@@ -0,0 +1,89 @@
+#   Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import parl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# clamp bounds for Std of action_log
+LOG_SIG_MAX = 2.0
+LOG_SIG_MIN = -20.0
+
+
+class GridModel(parl.Model):
+    def __init__(self, obs_dim, action_dim):
+        super(GridModel, self).__init__()
+        self.actor_model = Actor(obs_dim, action_dim)
+        self.critic_model = Critic(obs_dim, action_dim)
+
+    def policy(self, obs):
+        return self.actor_model(obs)
+
+    def value(self, obs, action):
+        return self.critic_model(obs, action)
+
+    def get_actor_params(self):
+        return self.actor_model.parameters()
+
+    def get_critic_params(self):
+        return self.critic_model.parameters()
+
+
+class Actor(parl.Model):
+    def __init__(self, obs_dim, action_dim):
+        super(Actor, self).__init__()
+
+        self.l1 = nn.Linear(obs_dim, 512)
+        self.l2 = nn.Linear(512, 256)
+        self.mean_linear = nn.Linear(256, action_dim)
+        self.std_linear = nn.Linear(256, action_dim)
+
+    def forward(self, obs):
+        x = F.relu(self.l1(obs))
+        x = F.relu(self.l2(x))
+
+        act_mean = self.mean_linear(x)
+        act_std = self.std_linear(x)
+        act_log_std = torch.clamp(act_std, min=LOG_SIG_MIN, max=LOG_SIG_MAX)
+        return act_mean, act_log_std
+
+
+class Critic(parl.Model):
+    def __init__(self, obs_dim, action_dim):
+        super(Critic, self).__init__()
+
+        # Q1 network
+        self.l1 = nn.Linear(obs_dim + action_dim, 512)
+        self.l2 = nn.Linear(512, 256)
+        self.l3 = nn.Linear(256, 1)
+
+        # Q2 network
+        self.l4 = nn.Linear(obs_dim + action_dim, 512)
+        self.l5 = nn.Linear(512, 256)
+        self.l6 = nn.Linear(256, 1)
+
+    def forward(self, obs, action):
+        x = torch.cat([obs, action], 1)
+
+        # Q1
+        q1 = F.relu(self.l1(x))
+        q1 = F.relu(self.l2(q1))
+        q1 = self.l3(q1)
+
+        # Q2
+        q2 = F.relu(self.l4(x))
+        q2 = F.relu(self.l5(q2))
+        q2 = self.l6(q2)
+        return q1, q2

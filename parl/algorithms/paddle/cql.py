17bc25232968f090c5e9fb284cb65c20e01b559b
bahe <bahe@baidu.com>
2022-01-17 11:27:59 +0800

    delete some annotations in cql.py

--- parl/algorithms/paddle/cql.py
+++ parl/algorithms/paddle/cql.py
@@ -71,20 +71,17 @@ class CQL(parl.Algorithm):
         self.tau = tau
         self.actor_lr = actor_lr
         self.critic_lr = critic_lr
-        self.sample_normal = Normal(loc=0., scale=1.)
         self.alpha_prime = 0.0
         self.policy_eval_start = policy_eval_start
-        self.with_automatic_entropy_tuning = True  #with_automatic_entropy_tuning
+        self.with_automatic_entropy_tuning = with_automatic_entropy_tuning
         self.with_lagrange = with_lagrange
         self.lagrange_thresh = lagrange_thresh
-        self.min_q_version = 3  #-1 if self.with_lagrange else min_q_version
+        self.min_q_version = -1 if self.with_lagrange else min_q_version
         self.min_q_weight = min_q_weight
         self.alpha = alpha
         self.temp = 1.0
         self.num_random = 10
         self._current_steps = 0
-
-        #self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model = model
         self.target_model = deepcopy(self.model)
         self.actor_optimizer = paddle.optimizer.Adam(
@@ -115,103 +112,16 @@ class CQL(parl.Algorithm):
     def sample(self, obs):
         """ Define the sampling process. This function returns an action with noise to perform exploration.
         """
-
-
-#   Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import parl
-import paddle
-import paddle.nn as nn
-import paddle.nn.functional as F
-
-# clamp bounds for Std of action_log
-LOG_SIG_MAX = 2.0
-LOG_SIG_MIN = -5.0
-
-
-class MujocoModel(parl.Model):
-    def __init__(self, obs_dim, action_dim):
-        super(MujocoModel, self).__init__()
-        self.actor_model = Actor(obs_dim, action_dim)
-        self.critic_model = Critic(obs_dim, action_dim)
-
-    def policy(self, obs):
-        return self.actor_model(obs)
-
-    def value(self, obs, action):
-        return self.critic_model(obs, action)
-
-    def get_actor_params(self):
-        return self.actor_model.parameters()
-
-    def get_critic_params(self):
-        return self.critic_model.parameters()
-
-
-class Critic(parl.Model):
-    def __init__(self, obs_dim, action_dim):
-        super(Critic, self).__init__()
-
-        # Q1 network
-        self.l1 = nn.Linear(obs_dim + action_dim, 256)
-        self.l2 = nn.Linear(256, 256)
-        self.l3 = nn.Linear(256, 256)
-        self.last_fc1 = nn.Linear(256, 1)
-
-        # Q2 network
-        self.l4 = nn.Linear(obs_dim + action_dim, 256)
-        self.l5 = nn.Linear(256, 256)
-        self.l6 = nn.Linear(256, 256)
-        self.last_fc2 = nn.Linear(256, 1)
-
-    def forward(self, obs, action):
-        x = paddle.concat([obs, action], 1)
-
-        # Q1
-        q1 = F.relu(self.l1(x))
-        q1 = F.relu(self.l2(q1))
-        q1 = F.relu(self.l3(q1))
-        q1 = self.last_fc1(q1)
-
-        # Q2
-        q2 = F.relu(self.l4(x))
-        q2 = F.relu(self.l5(q2))
-        q2 = F.relu(self.l6(q2))
-        q2 = self.last_fc2(q2)
-        return q1, q2
-
-
-class Actor(parl.Model):
-    def __init__(self, obs_dim, action_dim):
-        super(Actor, self).__init__()
-
-        self.l1 = nn.Linear(obs_dim, 256)
-        self.l2 = nn.Linear(256, 256)
-        self.l3 = nn.Linear(256, 256)
-        self.mean_linear = nn.Linear(256, action_dim)
-        self.std_linear = nn.Linear(256, action_dim)
-
-    def forward(self, obs):
-        x = F.relu(self.l1(obs))
-        x = F.relu(self.l2(x))
-        x = F.relu(self.l3(x))
-
-        act_mean = self.mean_linear(x)
-        act_std = self.std_linear(x)
-        act_log_std = paddle.clip(act_std, min=LOG_SIG_MIN, max=LOG_SIG_MAX)
-        return act_mean, act_log_std
+        act_mean, act_log_std = self.model.policy(obs)
+        normal = Normal(act_mean, act_log_std.exp())
+        # for reparameterization trick  (mean + std * N(0, 1))
+        x_t = normal.sample([1])
+        action = paddle.tanh(x_t)
+        log_prob = normal.log_prob(x_t)
+        # Enforcing Action Bound
+        log_prob -= paddle.log((1 - action.pow(2)) + 1e-6)
+        log_prob = paddle.sum(log_prob, axis=-1, keepdim=True)
+        return action[0], log_prob[0]
 
     def learn(self, obs, action, reward, next_obs, terminal):
         """ Define the loss function and create an optimizer to minize the loss.
@@ -301,6 +211,7 @@ class Actor(parl.Model):
         qf1_loss = qf1_loss + min_qf1_loss
         qf2_loss = qf2_loss + min_qf2_loss
         ## CQL done
+
         critic_loss = qf1_loss + qf2_loss
         self.critic_optimizer.clear_grad()
         critic_loss.backward(retain_graph=True)

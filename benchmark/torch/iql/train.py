5b97b8eb96f067046d92e92c4303d48e2224b0df
bahe <Heba_private@163.com>
2022-01-18 15:27:33 +0800

    upload iql

---
+++ benchmark/torch/iql/train.py
@@ -0,0 +1,125 @@
+#   Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import gym
+from tqdm import trange
+import d4rl
+from parl.utils import logger, tensorboard
+from replay_buffer import ReplayMemory
+from mujoco_model import MujocoModel
+from mujoco_agent import MujocoAgent
+from parl.algorithms import IQL
+import numpy as np
+EVAL_EPISODES = 10
+MEMORY_SIZE = int(2e6)
+BATCH_SIZE = 256
+
+
+# Runs policy for 5 episodes by default and returns average reward
+# A fixed seed is used for the eval environment
+def run_evaluate_episodes(agent, env, eval_episodes):
+    eval_returns = []
+    for _ in range(eval_episodes):
+        avg_reward = 0.
+        obs = env.reset()
+        done = False
+        while not done:
+            action = agent.predict(obs)
+            obs, reward, done, _ = env.step(action)
+            avg_reward += reward
+        eval_returns.append(avg_reward)
+    eval_returns = np.array(eval_returns)
+    avg_reward = eval_returns.mean()
+    return avg_reward, eval_returns
+
+
+def main():
+    logger.info("------------------- CQL ---------------------")
+    logger.info('Env: {}, Seed: {}'.format(args.env, args.seed))
+    logger.info("---------------------------------------------")
+
+    env = gym.make(args.env)
+    env.seed(args.seed)
+    obs_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.shape[0]
+    # Initialize model, algorithm, agent
+    model = MujocoModel(obs_dim, action_dim)
+    algorithm = IQL(model, max_steps=args.train_total_steps)
+    agent = MujocoAgent(algorithm)
+    # Initialize offline data
+
+    rpm = ReplayMemory(
+        max_size=MEMORY_SIZE, obs_dim=obs_dim, act_dim=action_dim)
+    rpm.load_from_d4rl(d4rl.qlearning_dataset(env))
+
+    model = MujocoModel(obs_dim, action_dim)
+    algorithm = IQL(model, max_steps=args.train_total_steps)
+    agent = MujocoAgent(algorithm)
+    total_steps = 0
+    test_flag = 0
+    result = []
+    for total_steps in trange(args.train_total_steps):
+        # Train steps
+        batch_obs, batch_action, batch_reward, batch_next_obs, batch_terminal = rpm.sample_batch(
+            BATCH_SIZE)
+        critic_loss, value_loss, actor_loss = agent.learn(
+            batch_obs, batch_action, batch_reward, batch_next_obs,
+            batch_terminal)
+        tensorboard.add_scalar('train/critic_loss', critic_loss, total_steps)
+        tensorboard.add_scalar('train/value_loss', value_loss, total_steps)
+        tensorboard.add_scalar('train/actor_loss', actor_loss, total_steps)
+        # Evaluate episode
+        if total_steps // args.test_every_steps >= test_flag:
+            while total_steps // args.test_every_steps >= test_flag:
+                test_flag += 1
+            avg_reward, eval_rewards = run_evaluate_episodes(
+                agent, env, EVAL_EPISODES)
+            normalized_returns = d4rl.get_normalized_score(
+                args.env, eval_rewards) * 100
+            normalized_mean = normalized_returns.mean()
+            tensorboard.add_scalar('eval/episode_reward' + str(i), avg_reward,
+                                   total_steps)
+            tensorboard.add_scalar('eval/episode_normalized_reward' + str(i),
+                                   normalized_mean, total_steps)
+            logger.info('Evaluation: total_steps {}, Reward: {}'.format(
+                total_steps, avg_reward))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--env",
+        default="hopper-medium-v2",
+        help='Mujoco gym environment name in d4rl')
+    parser.add_argument(
+        "--seed",
+        default=60,
+        type=int,
+        help='Sets Gym, PyTorch and Numpy seeds')
+    parser.add_argument(
+        "--train_total_steps",
+        default=int(1e6),
+        type=int,
+        help='Max time steps to run environment')
+    parser.add_argument(
+        '--test_every_steps',
+        type=int,
+        default=int(5e3),
+        help='The step interval between two consecutive evaluations')
+
+    args = parser.parse_args()
+    logger.info(args)
+
+    main()
---
+++ parl/algorithms/torch/iql.py
@@ -0,0 +1,117 @@
+#   Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import copy
+import torch
+import torch.nn as nn
+import parl
+import torch.nn.functional as F
+
+EXP_ADV_MAX = 100.
+
+
+def asymmetric_l2_loss(u, tau):
+    sign = (u < 0).float()
+    weight = (1 - sign) * tau + sign * (1 - tau)
+    return (weight * (u**2)).mean()
+
+
+class IQL(parl.Algorithm):
+    def __init__(self,
+                 model,
+                 max_steps,
+                 lr=0.0003,
+                 tau=0.7,
+                 beta=3.,
+                 discount=0.99,
+                 alpha=0.005):
+
+        self.device = torch.device("cuda" if torch.cuda.
+                                   is_available() else "cpu")
+        self.model = model.to(self.device)
+        self.q_target = copy.deepcopy(self.model).to(self.device)
+        self.lr = lr
+
+        self.v_optimizer = torch.optim.Adam(
+            self.model.get_value_params(), lr=self.lr)
+        self.q_optimizer = torch.optim.Adam(
+            self.model.get_critic_params(), lr=self.lr)
+        self.policy_optimizer = torch.optim.Adam(
+            self.model.get_actor_params(), lr=self.lr)
+        self.tau = tau
+        self.beta = beta
+        self.discount = discount
+        self.alpha = alpha
+
+    def predict(self, observations):
+        act = self.model.actor_model.act(observations, deterministic=True)
+        return act
+
+    def update(self, observations, actions, rewards, next_observations,
+               terminals):
+        with torch.no_grad():
+            target_q1, target_q2 = self.q_target.qvalue(observations, actions)
+            target_q = torch.min(target_q1, target_q2)
+            next_v = self.model.value(next_observations)
+
+        # Update value function
+        v = self.model.value(observations)
+        adv = target_q - v
+        v_loss = asymmetric_l2_loss(adv, self.tau)
+
+        # Update Q function
+        targets = rewards + (
+            1. - terminals.float()) * self.discount * next_v.detach()
+        q1, q2 = self.model.qvalue(observations, actions)
+        qf1_loss = F.mse_loss(q1, targets)
+        qf2_loss = F.mse_loss(q2, targets)
+        q_loss = qf1_loss + qf2_loss
+
+        # Update target Q network
+
+        # Update policy
+        exp_adv = torch.exp(self.beta * adv.detach()).clamp(max=EXP_ADV_MAX)
+        policy_out = self.model.policy(observations)
+        if isinstance(policy_out, torch.distributions.Distribution):
+            bc_losses = -policy_out.log_prob(actions)
+        policy_loss = torch.mean(exp_adv * bc_losses)
+
+        self.q_optimizer.zero_grad(set_to_none=True)
+        q_loss.backward()
+        self.q_optimizer.step()
+
+        self.v_optimizer.zero_grad(set_to_none=True)
+        v_loss.backward()
+        self.v_optimizer.step()
+
+        self.policy_optimizer.zero_grad(set_to_none=True)
+        policy_loss.backward()
+        self.policy_optimizer.step()
+
+        self.sync_target(alpha=self.alpha)
+        return q_loss.cpu().detach(), v_loss.cpu().detach(), policy_loss.cpu(
+        ).detach()
+
+    def sync_target(self, alpha=0):
+        """ update the target network with the training network
+        Args:
+            decay(float): the decaying factor while updating the target network with the training network.
+                        0 represents the **assignment**. None represents updating the target network slowly that depends on the hyperparameter `tau`.
+        """
+
+        for param, target_param in zip(self.model.parameters(),
+                                       self.q_target.parameters()):
+            target_param.data.copy_(alpha * param.data +
+                                    (1 - alpha) * target_param.data)
+        return None

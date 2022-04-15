f6303a7a8cccf2ce680a53c5321eb3609fb3ec3c
liuyixin-louis <zxcxzcz@qq.com>
2022-04-15 11:25:24 +0800

    delete_argument

--- benchmark/torch/maddpg/train.py
+++ benchmark/torch/maddpg/train.py
@@ -226,7 +226,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print('========== args: ', args)
     # logger.set_dir('./train_log/' + str(args.env))
-    logger.set_dir(args.log_dir + str(args.env) + '_' + str(args.seed) +
+    logger.set_dir('./train_log/' + str(args.env) + '_' + str(args.seed) +
                    '_' + str(args.continuous_actions))
 
     train_agent()

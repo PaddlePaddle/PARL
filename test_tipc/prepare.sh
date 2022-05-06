f806a7f1cbecf90382da988b8d87a1cdf5237f89
liuyixin-louis <zxcxzcz@qq.com>
2022-05-06 16:57:53 +0800

    update

--- test_tipc/prepare.sh
+++ test_tipc/prepare.sh
@@ -34,24 +34,33 @@ sed -i '/paddlepaddle/d' ./examples/${model_name}/requirements.txt
 ${python_name} -m pip install -r ./examples/${model_name}/requirements.txt
 sed '$ a paddlepaddle' ./examples/${model_name}/requirements.txt
 
+mojuco_envs="DDPG TD3 SAC PPO CQL"
 if [[ ${model_name} == "A2C" ]];then
-#  ${python_name} -m pip install paddlepaddle>=2.0.0
-#  ${python_name} -m pip install atari-py==0.1.7
-#  ${python_name} -m pip install parl>=1.4.3
-#  ${python_name} -m pip gym==0.12.1
   xparl stop
   xparl start --port 8010 --cpu_num 5
-#elif [ ${model_name} = "DQN" ];then
-#  ${python_name} -m pip install paddlepaddle>=2.0.0
-#  ${python_name} -m pip install gym
-#  ${python_name} -m pip install parl>=2.0.0
-#elif [ ${model_name} = "MADDPG" ];then
-#  ${python_name} -m pip install paddlepaddle>=2.0.0
-#  ${python_name} -m pip install parl>=2.0.4
-#  ${python_name} -m pip install PettingZoo==1.17.0
-#  ${python_name} -m pip install gym==0.23.1
-#elif [ ${model_name} = "QuickStart" ];then
-#  ${python_name} -m pip install gym
-#  ${python_name} -m pip install paddlepaddle>=2.0.0
-#  ${python_name} -m pip install parl>=2.0.0
+fi
+
+if [[ $mojuco_envs =~ $model_name ]]; then
+  # Get the prereqs
+  if [ ! -d "~/.mujoco" ]; then
+    apt-get -qq update
+    apt-get -qq install -y libosmesa6-dev libgl1-mesa-glx libglfw3 libgl1-mesa-dev libglew-dev patchelf
+    # Get Mujoco
+    mkdir ~/.mujoco
+    wget -q https://roboti.us/download/mjpro131_linux.zip -O mjpro131_linux.zip
+    unzip mjpro131_linux.zip -d "$HOME/.mujoco"
+    rm mjpro131_linux.zip
+    wget https://roboti.us/file/mjkey.txt
+    cp mjkey.txt ~/.mujoco
+    cp mjkey.txt ~/.mujoco/mjpro131/bin
+  #  echo 'export LD_LIBRARY_PATH=~/.mujoco/mjpro131/bin${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.bashrc
+  #  echo 'export MUJOCO_KEY_PATH=~/.mujoco${MUJOCO_KEY_PATH}' >> ~/.bashrc
+  #  echo 'export LD_PRELOAD=$LD_PRELOAD:/usr/lib/x86_64-linux-gnu/libGLEW.so' >> ~/.bashrc
+  #  echo "/root/.mujoco/mjpro131/bin" > /etc/ld.so.conf.d/mujoco_ld_lib_path.conf
+  #  ldconfig
+    ${python_name} -m pip install 'mujoco-py==0.5.7'
+  #  os.environ['LD_LIBRARY_PATH']=os.environ[''] + ':/root/.mujoco/mjpro131/bin'
+  #  os.environ['LD_PRELOAD']=os.environ['LD_PRELOAD'] + ':/usr/lib/x86_64-linux-gnu/libGLEW.so'
+  #  os.environ['LD_LIBRARY_PATH']=os.environ['LD_LIBRARY_PATH']+':/usr/lib/nvidia'
+  fi
 fi

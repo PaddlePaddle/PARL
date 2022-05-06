b7c20f91c8ae6d3ac467db32e392526a82ebec35
liuyixin-louis <zxcxzcz@qq.com>
2022-05-06 15:11:29 +0800

    dqn

--- test_tipc/test_train_inference_python.sh
+++ test_tipc/test_train_inference_python.sh
@@ -5,6 +5,9 @@ FILENAME=$1
 # MODE be one of ['lite_train_lite_infer' 'lite_train_whole_infer' 'whole_train_whole_infer', 'whole_infer', 'klquant_whole_infer']
 MODE=$2
 
+echo $FILENAME
+echo $MODE
+
 dataline=$(awk 'NR==1, NR==51{print}'  $FILENAME)
 
 # parser params

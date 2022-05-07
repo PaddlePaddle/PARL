9a94ed73a4c8e53a468c2004f8fd08a68e939807
liuyixin-louis <zxcxzcz@qq.com>
2022-05-07 12:29:48 +0800

    fixtipc

--- test_tipc/test_train_inference_python.sh
+++ test_tipc/test_train_inference_python.sh
@@ -66,7 +66,7 @@ mkdir -p ${LOG_PATH}
 status_log="${LOG_PATH}/results_python.log"
 
 # mujoco env var
-mojuco_envs="DDPG TD3 SAC PPO CQL"
+mojuco_envs="DDPG TD3 SAC PPO CQL ES"
 if [[ $mojuco_envs =~ $model_name ]]; then
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/.mujoco/mujoco210/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/.mujoco/mujoco200/bin

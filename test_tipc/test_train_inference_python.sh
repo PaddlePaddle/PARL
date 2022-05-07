4e3e63f8cf8870a9716cb71802390ff36520b903
liuyixin-louis <zxcxzcz@qq.com>
2022-05-07 12:52:42 +0800

    tipc

--- test_tipc/test_train_inference_python.sh
+++ test_tipc/test_train_inference_python.sh
@@ -67,10 +67,11 @@ status_log="${LOG_PATH}/results_python.log"
 
 # mujoco env var
 mojuco_envs="DDPG TD3 SAC PPO CQL ES OAC"
+echo $model_name
 if [[ $mojuco_envs =~ $model_name ]]; then
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/.mujoco/mujoco210/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/.mujoco/mujoco200/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/.mujoco/mjpro131/bin
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/.mujoco/mujoco210/bin
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/.mujoco/mujoco200/bin
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/.mujoco/mjpro131/bin
 fi
 
 function func_inference(){

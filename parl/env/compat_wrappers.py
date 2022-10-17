353f29bd46f03ab344bb2d4b8c3fb54f9d6f6863
Aidilele <115619013+Aidilele@users.noreply.github.com>
2022-10-17 14:03:28 +0800

    Update compat_wrappers.py

--- parl/env/compat_wrappers.py
+++ parl/env/compat_wrappers.py
@@ -16,61 +16,71 @@ import gym
 import numpy as np
 from parl.utils import logger
 
+# BASE_VERSION1 change env.seed() and env.reset()
+# BASE_VERSION2 change env.step()
+BASE_VERSION1 = [0, 26, 0]
+BASE_VERSION2 = [0, 25, 0]
+
 
 def get_gym_version():
     version = gym.__version__
-    start_flag = 0
-    for start_flag in range(len(version)):
-        if version[start_flag] == '.':
-            break
-    start_flag += 1
-    for end_flag in range(start_flag, len(version)):
-        if version[end_flag] == '.':
-            break
-    return int(version[start_flag:end_flag])
+    version_num = [int(x) for x in version.split('.')]
+    return version_num
+
+
+def compare_version(current_version_num, base_version_num):
+    if len(current_version_num) != len(base_version_num):
+        raise ValueError('Version format must be x.x.x')
+    else:
+        for i in range(len(current_version_num)):
+            if current_version_num[i] < base_version_num[i]:
+                return "Low"
+            elif current_version_num[i] > base_version_num[i]:
+                return 'HighEqual'
+    return 'HighEqual'
 
 
 class CompatWrapper(gym.Wrapper):
     def __init__(self, env):
-        """Map action space [-1, 1] of model output to new action space
-        [low_bound, high_bound].
+        """
+        Compat mujoco-v4
         """
 
-        gym.Wrapper.__init__(self, env)
+        super().__init__(env)
         attr_list = dir(env)
         if hasattr(env, '_max_episode_steps'):
             self._max_episode_steps = int(self.env._max_episode_steps)
         if hasattr(env, '_elapsed_steps'):
             self._elapsed_steps = self.env._elapsed_steps
         self.count_ep_step = 0
-        self.ramdom_seed = 0
+        self.ramdom_seed = 'without_setting'
 
     def reset(self, **kwargs):
-        if get_gym_version() >= 26:
-            kwargs['seed'] = self.ramdom_seed
+        if compare_version(get_gym_version(), BASE_VERSION1) == "HighEqual":
+            if self.ramdom_seed != "without_setting":
+                kwargs['seed'] = self.ramdom_seed
             obs, info = self.env.reset(**kwargs)
         else:
             obs = self.env.reset(**kwargs)
         return obs
 
     def seed(self, random_seed):
-        if get_gym_version() >= 26:
+        if compare_version(get_gym_version(), BASE_VERSION1) == "HighEqual":
             self.ramdom_seed = random_seed
         else:
             self.env.seed(random_seed)
 
-    def step(self, model_output_act):
-        """
-        Args:
-            model_output_act(np.array): The values must be in in [-1, 1].
-        """
-
+    def step(self, action):
         self.count_ep_step += 1
-        if get_gym_version() >= 25:
-            obs, reward, done, _, info = self.env.step(model_output_act)
+        if compare_version(get_gym_version(), BASE_VERSION2) == "HighEqual":
+            obs, reward, done, _, info = self.env.step(action)
+            if hasattr(self.env, '_elapsed_steps'):
+                self._elapsed_steps = self.env._elapsed_steps
         else:
-            obs, reward, done, info = self.env.step(model_output_act)
-        if self.count_ep_step >= 1000:
+            obs, reward, done, info = self.env.step(action)
+            if hasattr(env, '_elapsed_steps'):
+                self._elapsed_steps = self.env._elapsed_steps
+        if self.count_ep_step >= self._max_episode_steps:
             done = True
             self.count_ep_step = 0
         return obs, reward, done, info

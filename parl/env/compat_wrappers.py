c4e4dae42551fa53fe1a4d538a6bc146793792e3
Aidilele <115619013+Aidilele@users.noreply.github.com>
2022-11-03 17:32:14 +0800

    Update compat_wrappers.py

--- parl/env/compat_wrappers.py
+++ parl/env/compat_wrappers.py
@@ -1,4 +1,4 @@
-\#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,18 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import operator
-
 import gym
 import numpy as np
 from parl.utils import logger
+import operator
 
-# BASE_VERSION1 change env.seed() and env.reset()
-# BASE_VERSION2 change env.step()
-# BASE_VERSION3 change env.np_random from .randint() to .integers()
-BASE_VERSION1 = '0.26.0'
-BASE_VERSION2 = '0.25.0'
-BASE_VERSION3 = '0.21.0'
+# V_RESET_CHANGED change env.seed() and env.reset()
+# V_STEP_CHANGED change env.step()
+# V_NPRANDOM_CHANGED change env.np_random from .randint() to .integers()
+V_RESET_CHANGED = '0.26.0'
+V_STEP_CHANGED = '0.25.0'
+V_NPRANDOM_CHANGED = '0.21.0'
 
 
 def get_gym_version(version_str=gym.__version__):
@@ -51,7 +50,7 @@ class CompatWrapper(gym.Wrapper):
         self.random_seed = 'without_setting'
 
     def reset(self, **kwargs):
-        if operator.ge(get_gym_version(), get_gym_version(BASE_VERSION1)):
+        if operator.ge(get_gym_version(), get_gym_version(V_RESET_CHANGED)):
             if self.random_seed != "without_setting":
                 kwargs['seed'] = self.random_seed
             obs, info = self.env.reset(**kwargs)
@@ -60,14 +59,14 @@ class CompatWrapper(gym.Wrapper):
         return obs
 
     def seed(self, random_seed):
-        if operator.ge(get_gym_version(), get_gym_version(BASE_VERSION1)):
+        if operator.ge(get_gym_version(), get_gym_version(V_RESET_CHANGED)):
             self.random_seed = random_seed
         else:
             self.env.seed(random_seed)
 
     def step(self, action):
         self.count_ep_step += 1
-        if operator.ge(get_gym_version(), get_gym_version(BASE_VERSION2)):
+        if operator.ge(get_gym_version(), get_gym_version(V_STEP_CHANGED)):
             obs, reward, done, _, info = self.env.step(action)
             if hasattr(self.env, '_elapsed_steps'):
                 self._elapsed_steps = self.env._elapsed_steps

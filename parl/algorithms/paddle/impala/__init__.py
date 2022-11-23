1508ca38084b88510d60cd7448ec181eb4612932
Happy <lsb19@tsinghua.org.cn>
2022-11-23 21:04:51 +0800

    paddle version of impala (#993)

--- parl/algorithms/paddle/impala/__init__.py
+++ parl/algorithms/paddle/impala/__init__.py
@@ -1,4 +1,4 @@
-#   Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#   Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,16 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from parl.algorithms.paddle.policy_gradient import *
-from parl.algorithms.paddle.td3 import *
-from parl.algorithms.paddle.sac import *
-from parl.algorithms.paddle.dqn import *
-from parl.algorithms.paddle.ddpg import *
-from parl.algorithms.paddle.oac import *
-from parl.algorithms.paddle.qmix import *
-from parl.algorithms.paddle.a2c import *
-from parl.algorithms.paddle.ddqn import *
-from parl.algorithms.paddle.maddpg import *
-from parl.algorithms.paddle.ppo import *
-from parl.algorithms.paddle.cql import *
 from parl.algorithms.paddle.impala.impala import *

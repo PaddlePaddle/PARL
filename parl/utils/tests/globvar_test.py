df531dbac4738a6fc91c4affb8805581a97fdc1c
TomorrowIsAnOtherDay <2466956298@qq.com>
2022-11-24 14:35:34 +0800

    append global_config in the  module

---
+++ parl/utils/tests/globvar_test.py
@@ -0,0 +1,31 @@
+#   Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+import argparse
+from parl.utils import logger
+from parl.utils import global_config as config
+
+
+class TestGlobalConfig(unittest.TestCase):
+    def test_load_argument(self):
+        config.load_argument(parser.parse_args())
+        self.assertEqual(config.embed_dim, 128)
+        self.assertEqual(config.n_head, 1)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--embed_dim', type=int, default=128)
+    parser.add_argument('--n_head', type=int, default=1)
+    unittest.main()

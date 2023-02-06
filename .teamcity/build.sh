64d6b64bf8aa18ed37111b02948669e88e14f337
TomorrowIsAnOtherDay <2466956298@qq.com>
2023-02-06 16:51:18 +0800

    the default port for monitor_port depends on the cluster port

--- .teamcity/build.sh
+++ .teamcity/build.sh
@@ -270,8 +270,8 @@ function main() {
                     run_test_with_cpu $env
                     # uninstall paddlepaddle when testing remote module
                     pip uninstall -y paddlepaddle
-                    # run_test_with_cpu $env "DIS_TESTING_SERIALLY"
-                    # run_test_with_cpu $env "DIS_TESTING_REMOTE"
+                    run_test_with_cpu $env "DIS_TESTING_SERIALLY"
+                    run_test_with_cpu $env "DIS_TESTING_REMOTE"
                 else
                     echo ========================================
                     echo "in torch environment"

7ba206158d170c3eb5eb1f0f7ccc5b52a4fcbd93
rical730 <likejiao@baidu.com>
2023-02-08 12:33:43 +0800

    Update build.sh

--- .teamcity/build.sh
+++ .teamcity/build.sh
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 set -ex
+echo "WWWWWWWWWWWWWWWWWWWWWWW start"
 
 function init() {
     RED='\033[0;31m'

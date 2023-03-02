bce376021f30cd89e9b617c2af03db17de3c9fe7
TomorrowIsAnOtherDay <2466956298@qq.com>
2023-03-02 23:40:29 +0800

    kill all subprocess#18

--- parl/utils/test_utils.py
+++ parl/utils/test_utils.py
@@ -16,6 +16,7 @@ from parl.utils import get_free_tcp_port
 from parl.utils import is_port_available
 from parl.utils import logger
 import multiprocessing as mp
+import threading
 from parl.remote.master import Master
 from parl.remote.worker import Worker
 from parl.remote.client import disconnect
@@ -31,27 +32,22 @@ class XparlTestCase(unittest.TestCase):
     def setUp(self):
         self.port = get_free_tcp_port()
         self.ctx = mp.get_context()
-        self.sub_process = []
-        self.sub_process_type = []
         self.worker_process = []
+        self.master_exit_event = mp.Event()
 
     def tearDown(self):
         print('start tearDown')
-        assert len(self.sub_process) == len(self.sub_process_type)
-        for i, proc in enumerate(self.sub_process):
-            if self.sub_process_type[i] == 'master':
-                if proc.is_alive():
-                    proc.terminate()
-                    proc.join()
-            elif self.sub_process_type[i] == 'worker':
-                    os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
-            else:
-                raise NotImplementedError
+        self.master_exit_event.set()
+        self.remove_all_workers()
         disconnect()
 
     def _create_master(self, device):
         master = Master(port=self.port, device=device)
-        master.run()
+        th = threading.Thread(target=master.run)
+        th.setDaemon(True)
+        th.start()
+        self.master_exit_event.wait()
+        master.exit()
 
     def _create_worker(self, n_cpu, gpu):
         worker = Worker('localhost:{}'.format(self.port), n_cpu, None, gpu)
@@ -60,8 +56,6 @@ class XparlTestCase(unittest.TestCase):
     def add_master(self, device=remote_constants.CPU):
         p_master = self.ctx.Process(target=self._create_master, args=(device, ))
         p_master.start()
-        self.sub_process.append(p_master)
-        self.sub_process_type.append('master')
         while is_port_available(self.port):
             logger.info("Master[localhost:{}] starting".format(self.port))
             time.sleep(1)
@@ -76,8 +70,6 @@ class XparlTestCase(unittest.TestCase):
         time.sleep(3)
         p_worker = subprocess.Popen(command, close_fds=True, preexec_fn=os.setsid)
         time.sleep(2)
-        self.sub_process.append(p_worker)
-        self.sub_process_type.append('worker')
         self.worker_process.append(p_worker)
 
     def remove_all_workers(self):

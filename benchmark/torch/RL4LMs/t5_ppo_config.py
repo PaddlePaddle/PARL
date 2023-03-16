760cc9dcc844a4bb5cdf7200e09311f959727ec4
dwyzzy <805864608@qq.com>
2023-03-16 21:45:05 +0800

    change code style of PARL-RL4LMs summarization version 0.1

--- benchmark/torch/RL4LMs/t5_ppo_config.py
+++ benchmark/torch/RL4LMs/t5_ppo_config.py
@@ -1,3 +1,16 @@
+#   Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 config = {
     'tokenizer': {
@@ -74,25 +87,23 @@ config = {
                 'max_new_tokens': 100
             }
         },
-        'metrics': [
-            {
-                'id': 'meteor',
-                'args': {}
-            }, {
-                'id': 'rouge'
-            }, {
-                'id': 'bleu',
-                'args': {}
-            }, {
-               'id': 'bert_score',
-               'args': {
+        'metrics': [{
+            'id': 'meteor',
+            'args': {}
+        }, {
+            'id': 'rouge'
+        }, {
+            'id': 'bleu',
+            'args': {}
+        }, {
+            'id': 'bert_score',
+            'args': {
                 'language': 'en'
-               }
-            }, {
-                'id': 'diversity',
-                'args': {}
             }
-        ]
+        }, {
+            'id': 'diversity',
+            'args': {}
+        }]
     },
     'train_evaluation': {
         'n_iters': 100,
